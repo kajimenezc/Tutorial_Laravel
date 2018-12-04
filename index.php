08cef88ab10e2f75af10e499aaa00055597c7e28
keyblood97 <kevin.jimenezc97@gmail.com>
2018-12-04 11:14:49 -0500

    navbar vertical

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<title>BICI FIXIE</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
+	<!-- A vertical navbar -->
+<nav class="navbar bg-light">
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">HOME</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">INFO</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">GALLERY</a>
+    </li>
+  </ul>
+
+</nav>
 
 </body>
 </html>
